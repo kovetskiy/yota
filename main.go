01d062dc3bd82a5ba208a216aa99de8e40eea772
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-08-17 14:07:34 +0600

    refactoring: move username/password to config

--- main.go
+++ main.go
@@ -2,70 +2,97 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 
+	"github.com/BurntSushi/toml"
 	"github.com/docopt/docopt-go"
 	"github.com/kovetskiy/go-yota"
+	"github.com/zazab/zhash"
+)
+
+const (
+	usage = `yota-cli 2.0
+
+Usage:
+  yota-cli [options] -C -c <code>
+  yota-cli [options] -C -s <speed>
+  yota-cli [options] -L
+  yota-cli [options] -B
+  yota-cli -h | --help
+  yota-cli -v | --version
+
+Options:
+  -h --help      Show this help.
+  -v --version   Show version
+  -C             Change tariff.
+	-c  <code>   Specify tariff by code like 'POS-1234-567'
+	-s <speed>   Specify tariff by speed (float like '1.0' or just 'max')
+  -L             List all tariffs.
+  -B             Show balance.
+  -f <config>    Specify configuratin file [default: ~/.config/yotarc]
+`
 )
 
 func main() {
-	rawArgs := mergeArgsWithConfig(os.Getenv("HOME") + "/.config/yotarc")
-	args, err := parseArgs(rawArgs)
+	args, err := docopt.Parse(usage, nil, true, "2.0", true)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+		panic(err)
 	}
 
-	if args["--user"] == nil || args["--pass"] == nil {
-		fmt.Println("--user and --pass should be specified.")
-		os.Exit(1)
+	config, err := getConfig(args["-f"].(string))
+	if err != nil {
+		log.Fatalf("can't read config: %s", err)
 	}
 
-	username := args["--user"].(string)
-	password := args["--pass"].(string)
-
-	yotaCli := yota.NewClient(username, password, nil)
-
-	err = yotaCli.Login()
+	username, err := config.GetString("username")
 	if err != nil {
-		fmt.Printf("Could not login: %s", err.Error())
-		os.Exit(1)
+		log.Fatal(err)
+	}
+
+	password, err := config.GetString("password")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	yotaClient := yota.NewClient(username, password, nil)
+
+	err = yotaClient.Login()
+	if err != nil {
+		log.Fatalf("can't login: %s", err)
 	}
 
 	switch {
-	case args["list"]:
-		listMode(yotaCli)
+	case args["-L"].(bool):
+		err = listTariffs(yotaClient)
 
-	case args["switch"]:
-		query := map[string]string{
-			"Code":  "",
-			"Speed": "",
-		}
+	case args["-C"].(bool):
+		var (
+			code, _  = args["-c"].(string)
+			speed, _ = args["-s"].(string)
+		)
 
-		if args["--code"] != nil {
-			query["Code"] = args["--code"].(string)
-		} else if args["--speed"] != nil {
-			query["Speed"] = args["--speed"].(string)
-		}
+		err = searchAndChangeTariff(yotaClient, code, speed)
 
-		switchMode(yotaCli, query)
+	case args["-B"].(bool):
+		err = showBalance(yotaClient)
+	}
 
-	case args["balance"]:
-		balanceMode(yotaCli)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func listMode(yotaCli *yota.Client) {
-	tariffs, err := yotaCli.GetTariffs()
+func listTariffs(yotaClient *yota.Client) error {
+	tariffs, err := yotaClient.GetTariffs()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 
 	format := "%-12s %-5s %s\n"
-	fmt.Printf(format, "Code", "Speed", "Name")
+
+	fmt.Printf(format, "code", "speed", "name")
 	for _, tariff := range tariffs {
 		name := ""
 		if tariff.Active {
@@ -75,84 +102,62 @@ func listMode(yotaCli *yota.Client) {
 
 		fmt.Printf(format, tariff.Code, tariff.Speed, name)
 	}
+
+	return err
 }
 
-func balanceMode(yotaCli *yota.Client) {
-	balance, err := yotaCli.GetBalance()
+func showBalance(yotaClient *yota.Client) error {
+	balance, err := yotaClient.GetBalance()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 
-	fmt.Printf("%d", balance)
+	fmt.Printf("%d\n", balance)
+	return nil
 }
 
-func switchMode(yotaCli *yota.Client, query map[string]string) {
-	tariffs, err := yotaCli.GetTariffs()
+func searchAndChangeTariff(
+	yotaClient *yota.Client, code, speed string,
+) error {
+	tariffs, err := yotaClient.GetTariffs()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 
+	found := false
+	newTariff := yota.Tariff{}
 	for _, tariff := range tariffs {
-		found := tariff.Code == query["Code"] || tariff.Speed == query["--speed"]
-
-		if found {
-			err := yotaCli.ChangeTariff(tariff)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			fmt.Printf("Tariff successfully switched")
-			os.Exit(0)
+		if tariff.Code == code || tariff.Speed == speed {
+			found = true
+			newTariff = tariff
+			break
 		}
 	}
 
-	fmt.Printf("Something went wrong")
-	os.Exit(1)
-}
-
-func parseArgs(cmd []string) (map[string]interface{}, error) {
-	help := `Yota Cli.
-
-Usage:
-  yota-cli [options] list --user USERNAME --pass PASSWORD
-  yota-cli [options] switch (--code CODE|--speed SPEED) --user USERNAME --pass PASSWORD
-  yota-cli [options] balance --user USERNAME --pass PASSWORD
-  yota-cli -h | --help
-  yota-cli -v | --version
-
-Options:
-  -h --help           Show this help.
-  -v --version        Show version
-  --code=<code>       Set tariff by code like 'POS-1234-567'
-  --speed=<speed>     Set tariff by speed (float like '1.0' or just 'max')
-  --user=<username>   Your username in system
-  --pass=<password>   Your password in system
-`
-
-	args, err := docopt.Parse(help, cmd, true, "1.0", false)
-
-	return args, err
-}
-
-func mergeArgsWithConfig(path string) []string {
-	args := make([]string, 0)
-
-	conf, err := ioutil.ReadFile(path)
-	if err == nil {
-		confLines := strings.Split(string(conf), "\n")
-		for _, line := range confLines {
-			line = strings.TrimSpace(line)
-			if line == "" {
-				continue
-			}
-			args = append(args, line)
+	if found {
+		err = yotaClient.ChangeTariff(newTariff)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		fmt.Println("tariff successfully changed")
+		return nil
 	}
 
-	args = append(args, os.Args[1:]...)
+	return fmt.Errorf("can't find specified tariff")
+}
 
-	return args
+func getConfig(path string) (zhash.Hash, error) {
+	var configData map[string]interface{}
+
+	if strings.HasPrefix(path, "~/") {
+		path = strings.Replace(path, "~/", os.Getenv("HOME")+"/", 1)
+	}
+
+	_, err := toml.DecodeFile(path, &configData)
+	if err != nil {
+		return zhash.Hash{}, err
+	}
+
+	return zhash.HashFromMap(configData), nil
 }
